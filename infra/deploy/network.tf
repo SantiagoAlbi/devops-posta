84cafdc7edd4e116fb687c696a8968fdaf78a723
SantiagoAlbi <santi.albisetti@gmail.com>
2025-04-27 01:24:37 -0300

    added ec2 permissions

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -8,9 +8,9 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 }
 
-############################
-# Internet gateway
-##########################
+###################################################
+# Internet gateway needed for inbaund access to ALB
+###################################################
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
@@ -18,4 +18,130 @@ resource "aws_internet_gateway" "main" {
   tags = {
     Name = "${local.prefix}-main"
   }
+}
+
+###############################################
+# Public Subnet for load balancer public access
+###############################################
+
+resource "aws_subnet" "public_a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.1.1.0/24"
+  map_customer_owned_ip_on_launch = true
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+}
+
+resource "aws_route" "public_internet_access_a" {
+  route_table_id = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+############
+#subnet B
+###########
+
+resource "aws_subnet" "public_b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.1.2.0/24"
+  map_customer_owned_ip_on_launch = true
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.id
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+###############################################
+# Public Subnet for load balancer public access
+###############################################
+
+resource "aws_subnet" "private_a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
+}
+
+#####################################################################
+# Endpoints to allow ECS to access ECR, Cloudwatch and System Manager
+#####################################################################
+
+resource "aws_security_group" "endpoint_access" {
+  description = "Access endpoints"
+  name = "${local.prefix}-endpoint-access"
+  vpc_id = aws_vpc.main.id
+
+  ingress = {
+    cidr_block = [aws_vpc.main.cidr_block]
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr" {
+  vpc_id = aws_vpc.main.id
+  service_name = "com.amazon.${data.aws_region.current.name}.ecr.api"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+
+  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+
+  security_group_ids = [ 
+    aws_security_group.endpoint_access.id
+   ]
+
+   tags = {
+     Name = "${local.prefix}-ecr-endpoint"
+   }
 }
