f549baf2cce408ebe910f6c83e9d43e0112929d3
SantiagoAlbi <santi.albisetti@gmail.com>
2025-04-28 02:18:50 -0300

    added rds

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,12 @@ variable "contact" {
   description = "Contact emial for targgeting resources"
   default     = "santialbi@example.com"
 }
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
